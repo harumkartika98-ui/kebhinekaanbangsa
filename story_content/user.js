21e8bdc589ea8e4ae8d0c68b49b92766179e69c9
harumkartika98-ui <harumkartika98@gmail.com>
2026-05-07 16:39:25 +0700

    update

--- story_content/user.js
+++ story_content/user.js
@@ -2,6 +2,22 @@ function ExecuteScript(strId)
 {
   switch (strId)
   {
+      case "6UTr2SaaWvp":
+        Script1();
+        break;
   }
+}
+
+function Script1()
+{
+  var audio = document.getElementById('bgSong');
+
+audio.src = "1.mp3";
+
+audio.load();
+
+audio.play();
+
+audio.volume = 0.2;
 }
 
